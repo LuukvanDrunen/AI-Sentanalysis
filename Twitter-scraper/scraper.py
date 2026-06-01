6a087ea0bfc3999b80223e1bcde3d41608589db7
luukvandrunen <luukvandrunen@gmail.com>
2026-06-01 20:22:37 +0200

    added new scraper to use search_all

--- Twitter-scraper/scraper.py
+++ Twitter-scraper/scraper.py
@@ -8,29 +8,14 @@ load_dotenv()
 fields_to_include = ["article",
                      "attachments",
                      "author_id",
-                     "card_uri",
-                     "community_id",
-                     "context_annotations",
                      "conversation_id",
                      "created_at",
-                     "display_text_range",
-                     "edit_history_tweet_ids",
                      "entities",
                      "geo",
                      "id",
-                     "in_reply_to_user_id",
                      "lang",
                      "note_tweet",
-                     "possibly_sensitive",
-                     "public_metrics",
-                     "referenced_tweets",
-                     "reply_settings",
-                     "scopes",
-                     "source",
-                     "suggested_source_links",
-                     "suggested_source_links_with_counts",
-                     "text",
-                     "withheld"]
+                     "text"]
 client = Client(bearer_token=os.getenv('BEARER_TOKEN'))
 
 parser = argparse.ArgumentParser(description="Scraper CLI")
@@ -38,11 +23,13 @@ parser.add_argument("--userid", type=int, required=True)
 parser.add_argument("--n", type=int, default=10)
 parser.add_argument("--file", type=str)
 parser.add_argument("--username", type=str, default="")
+parser.add_argument("--since", type=str, default="")
+parser.add_argument("--until", type=str, default="")
 
 args = parser.parse_args()
+all_posts = []
 
-
-def scrape(userID, nPosts, file, username):
+def scrape(userID, nPosts, file, username, since, until):
     """Default scrape function"""
     if username:
         for uname in client.users.get_by_username(username=username, user_fields=["id"]):
@@ -50,8 +37,10 @@ def scrape(userID, nPosts, file, username):
                 userID = int(uname[1]['id'])
     for post in client.users.get_posts(id=userID, max_results=nPosts, tweet_fields=fields_to_include,
                                        exclude=['retweets'], expansions=['attachments.media_keys'],
-                                       media_fields=['url', 'variants']):
+                                       media_fields=['url', 'variants'], until_id=1730117810238464368):
         if post.data and len(post.data) > 0:
+            # page_data = getattr(post, 'data', []) or []
+            all_posts.extend(post.data)
             with open(file, 'w', encoding="UTF-8") as dump:
                 json.dump(post.model_dump(mode='json'), dump, indent=4, ensure_ascii=False)
             first_post = post.data[0]
@@ -61,7 +50,9 @@ def scrape(userID, nPosts, file, username):
         else:
             print("Something went wrong, no posts were found")
             break
+    with open("test.json", 'w', encoding="UTF-8") as dump:
+        json.dump(all_posts, dump, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    scrape(args.userid, args.n, args.file, args.username)
+    scrape(args.userid, args.n, args.file, args.username, args.since, args.until)
---
+++ Twitter-scraper/second-scraper.py
@@ -0,0 +1,57 @@
+import json
+import time
+import requests
+from xdk import Client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+client = Client(bearer_token=os.getenv('BEARER_TOKEN'))
+fields_to_include = ["article",
+                     "attachments",
+                     "author_id",
+                     "conversation_id",
+                     "created_at",
+                     "entities",
+                     "geo",
+                     "id",
+                     "lang",
+                     "note_tweet",
+                     "text"]
+# Search with automatic pagination
+count = len([name for name in os.listdir('.') if os.path.isfile(name)])
+save_file = 'serach_all.json'
+all_posts = []
+def scrape(id, date):
+    try:
+        for page in client.posts.search_all(
+            query='from:geertwilderspvv',
+            max_results=100,  # Per page
+            tweet_fields=fields_to_include,  # Optional expansions
+            expansions=['attachments.media_keys'],
+            media_fields=['url', 'variants'],
+            start_time='2016-06-01T00:00:00.000Z',
+            end_time=date
+        ):
+            all_posts.extend(page.data)
+            print(f"Fetched {len(page.data)} Posts (total: {len(all_posts)})")
+            last_post = page.data[-1]
+            last_post_date = last_post.created_at if hasattr(last_post, 'created_at') else last_post.get('created_at', '')
+            if hasattr(page, 'meta') and page.meta:
+                if hasattr(page.meta, 'oldest_id'):
+                    oldest_id = page.meta.oldest_id
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        print(f"Failed to fetch all posts, still dumping scraped tweets, if any were scraped")
+        if all_posts:
+            with open(str(save_file).replace('.json', f'-{str(count)}.json'), "w") as outfile:
+                json.dump(all_posts, outfile, indent=4, ensure_ascii=False)
+        print(f"Total tweets: {len(all_posts)}")
+        time.sleep((60 * 3))
+        scrape(oldest_id, last_post_date)
+
+
+
+if __name__ == "__main__":
+    scrape(1715728574643122658, '2023-10-21T13:55:43.000Z')
+# with open("search_all_success.json", "w") as outfile:
+#     json.dump(all_posts, outfile, indent=4, ensure_ascii=False)
