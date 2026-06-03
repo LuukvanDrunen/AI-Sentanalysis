8c3fad6e480d43a55fc085efd813e1303252113d
luukvandrunen <luukvandrunen@gmail.com>
2026-06-03 13:28:01 +0200

    changes some stuff around

--- Sentiment Analysis Model/ai-rate-tweets.py
+++ Sentiment Analysis Model/ai-rate-tweets.py
@@ -4,22 +4,25 @@ import csv
 import re
 
 # text_only_file = '../Results/Geert Wilders/Final-Wilders-Results.json'
-text_only_file = '/home/luukvandrunen/OneDrive/Universiteit Utrecht/AI-Sentanalysis/Results/Geert Wilders/Final-Wilders-Results.json'
+text_only_file = '/home/luukvandrunen/OneDrive/Universiteit Utrecht/AI-Sentanalysis/Results/Geert Wilders/Part1-Final-Wilders.json'
 
-csv_filename = 'ai_labeled.csv'
+csv_filename = 'Wilders_part1_labeled.csv'
 with open(text_only_file) as f:
     data = json.load(f)
+data = list(data)
 
-model = AutoModelForSequenceClassification.from_pretrained("sytnaxerror/robbert-L-dbrd-imdb-clap")
-tokenizer = AutoTokenizer.from_pretrained("sytnaxerror/robbert-L-dbrd-imdb-clap")
+model = AutoModelForSequenceClassification.from_pretrained("/home/luukvandrunen/.cache/huggingface/hub/models--sytnaxerror--robbert-L-dbrd-imdb-clap/snapshots/d070221cafb2b11bbfba1405431f8056efa8a479")
+tokenizer = AutoTokenizer.from_pretrained("/home/luukvandrunen/.cache/huggingface/hub/models--sytnaxerror--robbert-L-dbrd-imdb-clap/snapshots/d070221cafb2b11bbfba1405431f8056efa8a479")
 pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
 
 labeled_data = pipe(data)
 final_dict = {}
 
 for i in range(len(data)):
-    final_dict[data[i]] = int(re.search(r'\d+', labeled_data[i]['label']).group())
+    final_dict[data[i]['text']] = int(re.search(r'\d+', labeled_data[i]['label']).group())
 
+# for i in data:
+#     final_dict[i['text']] = int(re.search(r'\d+', labeled_data[i]['label']).group())
 
 def save():
     with open(csv_filename, mode='w', newline='') as f:
